25cafc7be417d5c0879317f638b27c518f9f9b28
caro2904 <caro_ramirez0429@hotmail.com>
2021-01-19 21:52:34 -0600

    V1 muestra

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyC4RNNlqXSwTM1Kt6Pbgutq2yuavSQ9WnU',
+    authDomain: 'caro2904-angularproofapp.firebaseapp.com',
+    databaseURL: 'http://jnovoa.pythonanywhere.com/podemos/',
+    projectId: 'caro2904-angularproofapp',
+    storageBucket: 'caro2904-angularproofapp.appspot.com',
+    messagingSenderId: '965114235515'
+  }
+};
